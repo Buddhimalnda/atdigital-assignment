daa461e3689f3eb2bd561102853ae48b39f020b0
Buddhimalnda <buddhimalinda66@gmail.com>
2024-04-20 03:16:14 +0530

    fixed align issu

--- components/services/index.tsx
+++ components/services/index.tsx
@@ -12,10 +12,7 @@ const Service = (props: ServiceProps) => {
   const route = useRouter();
   return (
     <div
-      className={
-        "service " +
-        (props.isRightSideImg ? "flex-row-reverse" : "justify-between")
-      }
+      className={"service " + (props.isRightSideImg ? "flex-row-reverse " : "")}
     >
       <div
         className={"img  " + (props.isRightSideImg ? "md:ml-52" : "md:mr-52")}
